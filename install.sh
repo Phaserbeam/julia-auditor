9a1149e274af2fcc717034d65344f0e8f4bcdb62
Zypeth <68081252+Phaserbeam@users.noreply.github.com>
2025-08-30 22:55:41 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#! /bin/zsh
+
+INSTALL_NVIM=false
+while getopts "n" opt; do
+  case $opt in
+    n) INSTALL_NVIM=true;;
+    *) echo "Invalid parameter!"
+      exit 1;;
+  esac
+done
+
+
+mkdir -p ~/.bin
+curl -L -o ~/.bin https://github.com/Phaserbeam/julia-auditor/releases/latest/download/auditor
+chmod +x ~/.bin/auditor
+
+if $INSTALL_NVIM; then
+  mkdir -p ~/.config/nvim
+  echo "vim.keymap.set('n', '<leader>j', '<cmd>silent ! ~/.bin/auditor %<cr><cmd>edit<cr>',
+  { desc = 'Audit file' })" >> ~/.config/nvim/init.lua
+fi
+
+echo "Installation complete!"
